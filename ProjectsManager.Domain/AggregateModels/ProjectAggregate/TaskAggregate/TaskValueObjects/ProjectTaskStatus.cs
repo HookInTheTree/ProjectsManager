1f124d858bd1c4290c0ee0e2ef4026a6091190d0
Chera Dz <dzheriev2003@mail.ru>
2023-10-17 08:38:47 +0300

    Added Enumeration abstraction. Also i realized logic of ProjectTask states

---
+++ ProjectsManager.Domain/AggregateModels/ProjectAggregate/TaskAggregate/TaskValueObjects/ProjectTaskStatus.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjectsManager.Domain.AggregateModels.ProjectAggregate.TaskAggregate.TaskValueObjects;
+
+public class ProjectTaskStatus:Enumeration
+{
+    public static ProjectTaskStatus Draft = new(1, nameof(Draft));
+    public static ProjectTaskStatus Activated = new(1, nameof(Activated));
+    public static ProjectTaskStatus Completed = new(1, nameof(Completed));
+    public static ProjectTaskStatus Verified = new(1, nameof(Verified));
+    public static ProjectTaskStatus Rejected = new(1, nameof(Rejected));
+    public ProjectTaskStatus(int id, string name) : base(id, name)
+    {
+    }
+}

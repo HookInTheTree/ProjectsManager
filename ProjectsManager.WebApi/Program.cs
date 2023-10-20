9886f6f970f9f5026504029ff600d67c2523cebe
Chera Dz <dzheriev2003@mail.ru>
2023-10-20 17:55:31 +0300

    Try to init migrations

--- ProjectsManager.WebApi/Program.cs
+++ ProjectsManager.WebApi/Program.cs
@@ -1,6 +1,9 @@
+using ProjectsManager.Infrastructure;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddInfrastructure(new InfrastructureOptions(builder.Configuration["ConnectionStrings:Database"].ToString()));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle

9a2c30617585fa0e3a1b27178a17b9be4a559188
Chera Dz <dzheriev2003@mail.ru>
2023-11-08 17:12:01 +0300

    WebApi. Just add some endpoint in default controller to check that my project works

--- ProjectsManager.WebApi/Controllers/WeatherForecastController.cs
+++ ProjectsManager.WebApi/Controllers/WeatherForecastController.cs
@@ -1,9 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
+using ProjectsManager.Domain.Common.ValueObjects;
+using ProjectsManager.Domain.EmployeeAggregate.ValueObjects;
+using ProjectsManager.Domain.ProjectAggregate.ValueObjects;
+using ProjectsManager.Domain.WorkItem;
+using ProjectsManager.Domain.WorkItem.Repositories;
+using ProjectsManager.Domain.WorkItem.ValueObjects;
 
 namespace ProjectsManager.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]/[action]")]
 public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
@@ -12,21 +18,46 @@ public class WeatherForecastController : ControllerBase
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
+    private readonly IWorkItemRepository workItemRepository;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    public WeatherForecastController(ILogger<WeatherForecastController> logger, IWorkItemRepository repo)
     {
         _logger = logger;
+        workItemRepository = repo;
     }
 
-    [HttpGet(Name = "GetWeatherForecast")]
+    [HttpGet(Name = "GetWeather")]
     public IEnumerable<WeatherForecast> Get()
     {
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
             .ToArray();
+    }
+
+    [HttpGet(Name = "GetWorkItems")]
+    public async Task<List<WorkItem>> GetWorkItems()
+    {
+        return await workItemRepository.GetAll();
+    }
+
+
+    [HttpPost(Name = "CreateWorkItem")]
+    public async Task<WorkItem> CreateWork()
+    {
+        var workItem = new WorkItem(
+            WorkItemId.CreateUnique(),
+            new Name("145234523452345234514523452345234523451452345234523452345145234523452345234514523452345234523451452345234523452345"),
+            new Description("12341234123412342134123414523452345234523451452345234523452345145234523452345234514523452345234523451452345234523452345"),
+            new Duration(DateTime.Now, DateTime.Now.AddDays(1)),
+            ProjectId.CreateUnique(),
+            EmployeeId.CreateUnique());
+
+        await workItemRepository.Insert(workItem);
+        await workItemRepository.Save();
+        return workItem;
     }
 }
